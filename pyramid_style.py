bb0a97151699306ef5cc8fcd3898f858d5e049b0
kishor9581 <kishorpatil9581@gmail.com>
2024-04-11 09:35:24 +0530

    *`s in pyramid style

--- pyramid_style.py
+++ pyramid_style.py
@@ -1,8 +1,11 @@
-def pyramid(num):
-    for i in range(1, num + 1):
-        print(" " * (num - i), end="")
-        print("* " * i)
+try:
+    def pyramid(num):
+        for i in range(1, num + 1):
+            print(" " * (num - i), end="")
+            print("* " * i)
 
 
-n = int(input("Enter number of rows : "))
-pyramid(n)
+    n = int(input("Enter number of rows : "))
+    pyramid(n)
+except Exception as e:
+    print("Error found ", e)
